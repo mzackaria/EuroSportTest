08b06db807b83f8c76921b10f27b62c3afc31815
Zakaria MESSAI <messai.zackaria@gmail.com>
2023-01-15 23:33:50 +0100

    added handler for internet error state and loading state

--- app/src/main/java/com/mdroid/eurosporttest/ui/main/MainScreen.kt
+++ app/src/main/java/com/mdroid/eurosporttest/ui/main/MainScreen.kt
@@ -6,10 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
@@ -45,6 +42,13 @@ fun MainScreen(
         content = {
             when(responseListOfNews) {
                 is Response.Success -> NewsList(navController, responseListOfNews.data ?: emptyList())
+                is Response.Error -> Text(
+                    text = stringResource(id = R.string.internet_error),
+                    modifier = Modifier.fillMaxSize(),
+                    textAlign = TextAlign.Center
+
+                )
+                is Response.Loading -> LinearProgressIndicator()
             }
 
         }
