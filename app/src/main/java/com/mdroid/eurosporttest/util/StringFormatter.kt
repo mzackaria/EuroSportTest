00c9622de38405f06272f1cfcb06a7def3734ef4
Zakaria MESSAI <messai.zackaria@gmail.com>
2023-01-22 21:29:26 +0100

    changed the since format method

--- app/src/main/java/com/mdroid/eurosporttest/util/StringFormatter.kt
+++ app/src/main/java/com/mdroid/eurosporttest/util/StringFormatter.kt
@@ -1,12 +1,11 @@
 package com.mdroid.eurosporttest.util
 
-import android.text.format.DateUtils
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.mdroid.eurosporttest.R
-import java.util.*
-import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.DurationUnit
 
 @Composable
 fun formatLabelNumberViews(numberOfViews: Int): String {
@@ -19,30 +18,26 @@ fun formatLabelStories(author: String, date: Double): String {
     return stringResource(id = R.string.format_label_card_story, author, timeString)
 }
 
-//TODO REFAIRE CE TRUC
 @Composable
 fun sinceFormat(date: Double): String {
 
+    val formatSinceString = stringResource(id = R.string.format_label_since)
+
     val currentTimestamp = System.currentTimeMillis()
+    val diffDuration: Duration = (currentTimestamp - (date * 1000.0).toLong()).milliseconds
 
-    val diff: Long = currentTimestamp - (date * 1000.0).toLong()
-    val seconds = diff / 1000
-    val minutes = seconds / 60
-    val hours = minutes / 60
-    val days = hours / 24
-    val years = days / 365
+    val days = diffDuration.inWholeDays
+    val hours = diffDuration.inWholeHours
+    val minutes = diffDuration.inWholeMinutes
+    val seconds = diffDuration.inWholeSeconds
 
-    if (hours == 0L && days == 0L && years == 0L) {
-        val unit = stringResource(id = R.string.minutes)
-        return stringResource(id = R.string.format_label_since).format(minutes, unit)
-    } else if (days == 0L && years == 0L) {
-        val unit = stringResource(id = R.string.hours)
-        return stringResource(id = R.string.format_label_since).format(hours, unit)
-    } else if (years == 0L) {
-        val unit = stringResource(id = R.string.day)
-        return stringResource(id = R.string.format_label_since).format(days, unit)
-    } else {
-        val unit = stringResource(id = R.string.years)
-        return stringResource(id = R.string.format_label_since).format(years, unit)
-    }
+    return formatSinceString.format(
+         when {
+            days > 364 -> (days / 365).toString() + stringResource(id = R.string.years)
+            hours > 23 -> diffDuration.toString(DurationUnit.DAYS)
+            minutes > 59 -> diffDuration.toString(DurationUnit.HOURS)
+            seconds > 59 -> diffDuration.toString(DurationUnit.MINUTES)
+            else -> stringResource(id = R.string.a_few_seconds_label)
+        }
+    )
 }
